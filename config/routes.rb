fb3610d10e27c499dc2ead4974deb9851879fdf5
YuriNakano <yuriinlethbridge@outlook.com>
2023-04-16 17:36:15 +0000

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  # devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'public/homes#top'
   
-  
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
 end
