528c8d20e08765b9238fb4df4843b75800f089f6
YuriNakano <yuriinlethbridge@outlook.com>
2023-04-16 17:53:26 +0000

    Make it possible to log in as admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'public/homes#top'
+  get '/admin' => 'admin/homes#top', as: 'admin_top'
   
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
